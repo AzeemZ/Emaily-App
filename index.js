f73ec97786c7d317a24b6e5b91fdc8d8d6bed654
AzeemZ <azeem3z123@gmail.com>
2019-10-25 15:28:47 +0500

    Serve react assets

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieSession = require("cookie-session");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const path = require("path");
 const { mongoURI, cookieKey } = require("./config/keys");
 require("./services/passport");
 const authRouter = require("./routes/auth");
@@ -28,6 +29,14 @@ app.use(passport.session());
 
 app.use(authRouter);
 app.use(billingRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
