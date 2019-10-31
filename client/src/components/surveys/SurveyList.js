7185306f53f27fc56f56adaab58b7bcafdd2c20e
AzeemZ <azeem3z123@gmail.com>
2019-10-31 20:36:42 +0500

    Display message if no survey created

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import { fetchSurveys } from "actions";
 
@@ -9,32 +10,39 @@ const SurveyList = ({ surveys, fetchSurveys }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const surveysList = () => {
-    if (!surveys.length) {
+  const listOfSurveys = () => {
+    if (!surveys) {
       return;
+    } else if (!surveys.length) {
+      return (
+        <h5 style={{ marginTop: 40 }}>
+          You have not created any surveys yet. Would you like to{" "}
+          <Link to="/surveys/new">create one</Link>?
+        </h5>
+      );
+    } else {
+      return surveys.reverse().map(survey => (
+        <div className="card darken-1">
+          <div className="card-content">
+            <span className="card-title">
+              <strong>{survey.title}</strong>
+            </span>
+            <p>{survey.body}</p>
+          </div>
+          <div className="card-action">
+            <a href="/surveys">YES: {survey.yes}</a>
+            <a href="/surveys">NO: {survey.no}</a>
+            <span className="right">
+              <strong>Sent On:</strong>{" "}
+              {new Date(survey.dateSent).toLocaleDateString()}
+            </span>
+          </div>
+        </div>
+      ));
     }
-
-    return surveys.reverse().map(survey => (
-      <div className="card darken-1">
-        <div className="card-content">
-          <span className="card-title">
-            <strong>{survey.title}</strong>
-          </span>
-          <p>{survey.body}</p>
-        </div>
-        <div className="card-action">
-          <a href="/surveys">YES: {survey.yes}</a>
-          <a href="/surveys">NO: {survey.no}</a>
-          <span className="right">
-            <strong>Sent On:</strong>{" "}
-            {new Date(survey.dateSent).toLocaleDateString()}
-          </span>
-        </div>
-      </div>
-    ));
   };
 
-  return <div>{surveysList()}</div>;
+  return <div>{listOfSurveys()}</div>;
 };
 
 const mapStateToProps = ({ surveys }) => {
